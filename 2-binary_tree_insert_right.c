530cf3107493a8b23e86a70609636efb3e40e023
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-09-21 21:56:34 +0000

    inserts a node as the right-child of another node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,19 +5,26 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	if (parent == NULL)
+
 	{
-		return(NULL);
+
+		return (NULL);
+
 	}
 
 	node = binary_tree_node(parent, value);
 	if (node == NULL)
+
 	{
-		return(NULL);
+
+		return (NULL);
+
 	}
-	if (parent ->right != NULL)
+	if (parent->right != NULL)
+
 	{
-		node ->right = parent ->right;
-		parent ->right -> parent = node;
+		node->right = parent->right;
+		parent->right->parent = node;
 	}
 
 	parent->right = node;
